f2687e0e2813595e6c153081728d9d3857a91393
gliese-sketch <173663566+gliese-sketch@users.noreply.github.com>
2025-02-11 13:28:31 +0000

    show loading on read state

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,11 +1,26 @@
 "use client";
-import {  HeroUIProvider } from "@heroui/react";
+import { useState } from "react";
+import { HeroUIProvider } from "@heroui/react";
 import { Messages, Inputs, Chat, SignUp } from "@/components";
+import { io } from "socket.io-client";
+
+const socket = io(
+  "https://bug-free-space-fishstick-jj4q6gx9rg9q2j66v-8000.app.github.dev/"
+);
 
 export default function Home() {
+  const [user, setUser] = useState("");
+
   return (
     <HeroUIProvider>
-    <SignUp/>
-     </HeroUIProvider>
+      {user ? (
+        <>
+          <Messages />
+          <Inputs />
+        </>
+      ) : (
+        <SignUp setUser={setUser} socket={socket} />
+      )}
+    </HeroUIProvider>
   );
 }
